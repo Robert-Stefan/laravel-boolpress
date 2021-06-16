22182469c131d8c2bd5425fc8c331edc2037a66c
Robert-Stefan <robertboss05@hotmail.it>
2021-06-16 16:20:49 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
+// ROTTE X AUTENTICAZIONE
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/admin', 'HomeController@index')->name('home');
+
+// ROUTE ADMIN
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin')
+    ->group(function() {
+        // ROUTE HOME ADMIN
+        Route::get('/', 'HomeController@index')->name('home');
+});

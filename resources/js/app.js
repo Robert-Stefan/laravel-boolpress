5c8f6c41cd4d8277181104030a3d8f2b8870ecd7
Robert-Stefan <robertboss05@hotmail.it>
2021-06-17 11:04:04 +0200

    Add errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,9 +9,9 @@ require("./bootstrap");
 window.Vue = require("vue");
 
 // INIT VUE MAIN INSTANCE
-import App from "./App.vue";
+//import App from "./App.vue";
 
-const root = new Vue({
-    el: "#root",
-    render: h => h(App)
-});
+//const root = new Vue({
+//    el: "#root",
+//    render: h => h(App)
+//});
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,30 +4,41 @@
     <div class="container">
         <h1 class="mb-5">CREATE NEW POST</h1>
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        
 
         <div class="row">
+            
+
             <div class="col-md-8 offset-md-2">
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
                 <form action="{{ route('adminposts.store') }}" method="POST">
                     @csrf
                     @method('POST')
 
                     <div class="mb-3">
                         <label for="title" class="form-label">Title*</label>
-                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" value="{{ old('title') }}">
+                        @error('title')
+                            <div class="feedback">XXX {{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="mb-3">
                         <label for="content" class="form-label">Content*</label>
-                        <textarea class="form-control" name="content" id="content" rows="6">{{ old('content') }}</textarea>
+                        <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" rows="6">{{ old('content') }}</textarea>
+                        @error('content')
+                            <div class="feedback">YYY {{ $message }}</div>
+                        @enderror
                     </div>
 
                     <button class="btn btn-primary" type="submit">Create post</button>

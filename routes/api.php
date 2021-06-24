37306e2268b161e7e43f7566a14891f60cb27a3a
Robert-Stefan <robertboss05@hotmail.it>
2021-06-24 10:55:06 +0200

    Add return json

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/test', function() {
+
+    return response()->json([
+        'names' => ['Paolo', 'Luca', 'Michela', 'Chiara'],
+        'lorem' => 'ipsum',
+    ]);
+});
+
+// GET BLOG POSTS 
+Route::namespace('Api')->group(function() {
+    //Get posts
+    Route::get('/posts', 'PostController@index');
 });
